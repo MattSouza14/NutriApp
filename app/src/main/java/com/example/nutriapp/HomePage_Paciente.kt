0ba5b2fee74df0133ee63f5f3272c779356471db
iarley <iarleymordekay@gmail.com>
2025-01-11 20:01:45 -0300

    Criação da tela provisoria de medicos e adição de troca entre fragments

--- app/src/main/java/com/example/nutriapp/HomePage_Paciente.kt
+++ app/src/main/java/com/example/nutriapp/HomePage_Paciente.kt
@@ -1,13 +1,15 @@
 package com.example.nutriapp
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.example.nutriapp.Telas_home.Home_Fragment
+import com.example.nutriapp.Telas_home.Medicos
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomePage_Paciente : AppCompatActivity() {
@@ -15,9 +17,41 @@ class HomePage_Paciente : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home_page_paciente)
-        supportFragmentManager
-            .beginTransaction()
-            .add(R.id.conteudo,Home_Fragment())
+        //inicia a tela com Homepage_Fragment
+
+        //Criação de uma varievel que recebe o principal constraint
+
+        //Criação de uma variavel com Acesso a Barra Inferior
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.BarraInferior)
+
+        //Case para a navegação
+        bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    replaceFragment(Home_Fragment())
+                    true
+                }
+                R.id.nav_search -> {
+                    replaceFragment(Medicos())
+                    true
+
+                }
+
+            //Uso Futuro, NÃO MEXER AINDA
+//               R.id.nav_profile -> {
+//                    replaceFragment(ProfileFragment())
+//                    true
+//                }
+                else -> false
+            }
+        }
+        // Carregar o fragmento inicial
+        bottomNavigation.selectedItemId = R.id.nav_home
+    }
+    //Função para troca de Fragments usando a Barra Inferior
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.conteudo, fragment)
             .commit()
     }
 }
---
+++ app/src/main/java/com/example/nutriapp/Telas_home/Medicos.kt
@@ -0,0 +1,21 @@
+package com.example.nutriapp.Telas_home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.nutriapp.R
+
+class Medicos : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_medicos, container, false)
+    }
+
+}
