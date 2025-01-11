f74f6165363401268a919fe3b4e7231103c0ab28
iarley <iarleymordekay@gmail.com>
2025-01-10 22:39:20 -0300

    Mudança na HomePage e criação/Adição de Fragments

--- app/src/main/java/com/example/nutriapp/HomePage_Paciente.kt
+++ app/src/main/java/com/example/nutriapp/HomePage_Paciente.kt
@@ -1,20 +1,23 @@
 package com.example.nutriapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.nutriapp.Telas_home.Home_Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomePage_Paciente : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home_page_paciente)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.conteudo,Home_Fragment())
+            .commit()
     }
 }
--- app/src/main/java/com/example/nutriapp/Login.kt
+++ app/src/main/java/com/example/nutriapp/Login.kt
@@ -1,6 +1,8 @@
 package com.example.nutriapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,5 +14,11 @@ class Login : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_login_paciente)
 
+        val telaLogin = findViewById(R.id.entrar) as Button
+
+        telaLogin.setOnClickListener{
+            val trocaTela = Intent(this, HomePage_Paciente::class.java)
+            startActivity(trocaTela)
+        }
     }
 }
